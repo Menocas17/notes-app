676a72d08b965f064c44fe22cd5b93d172b73acc
Menocas17 <rodolfomenocal7@gmail.com>
2026-02-24 14:33:05 -0600

    improved the UX to show the right icon accordign to the theme selected

--- frontend/src/components/ThemeToggler.tsx
+++ frontend/src/components/ThemeToggler.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { useTheme } from '../context/ThemeContext';
 import { type Theme } from '../context/ThemeContext';
 
+const themeOptions = ['light', 'dark', 'system'];
+
 export default function ThemeToggler() {
   const [isOpen, setIsOpen] = useState(false);
   const { theme, toggleTheme } = useTheme();
@@ -20,57 +22,42 @@ export default function ThemeToggler() {
         }}
         className='cursor-pointer'
       >
-        <img src={'/themeSelectorIcon.svg'} alt='Theme Icon' width={30} />
+        <img
+          src='/themeSelectorBlackBorderIcon.svg'
+          alt='Theme Icon'
+          width={30}
+          className='block dark:hidden'
+        />
+
+        <img
+          src='/themeSelectorIcon.svg'
+          alt='Theme Icon'
+          width={30}
+          className='hidden dark:block'
+        />
       </button>
 
       {isOpen && (
-        <div className='absolute right-0 z-10 mt-2 w-30 origin-top-right rounded-md bg-gray-100 border border-gray-200  text-black dark:bg-[#242424] dark:border dark:border-[#6A7282]'>
-          <div className='py-1 flex flex-col'>
-            <button
-              onClick={handleClick}
-              className=' w-full px-4 py-2 text-left text-sm font-bold text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-[#1A1A1A] flex gap-2 items-center '
-              value={'light'}
-            >
-              <img
-                src={
-                  theme === 'light'
-                    ? '/lightBorderBlackIcon.svg'
-                    : 'lightIcon.svg'
-                }
-                alt='Theme Icon'
-              />
-              Light
-            </button>
-            <button
-              onClick={handleClick}
-              className={`w-full px-4 py-2 text-left text-sm font-bold ${theme === 'dark' ? 'bg-[#1A1A1A]' : ''} text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-[#1A1A1A] flex gap-2 items-center`}
-              value={'dark'}
-            >
-              <img
-                src={
-                  theme === 'light'
-                    ? '/darkBorderBlackIcon.svg'
-                    : 'darkIcon.svg'
-                }
-                alt='Theme Icon'
-              />
-              Dark
-            </button>
-            <button
-              onClick={handleClick}
-              className='w-full px-4 py-2 text-left text-sm font-bold text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-[#1A1A1A] flex gap-2 items-center'
-              value={'system'}
-            >
-              <img
-                src={
-                  theme === 'light'
-                    ? '/systemBorderBlackIcon.svg'
-                    : 'systemIcon.svg'
-                }
-                alt='Theme Icon'
-              />
-              System
-            </button>
+        <div className='absolute right-0 z-10 mt-2 w-30 origin-top-right rounded-lg bg-gray-100 border border-gray-200  text-black dark:bg-[#242424] dark:border dark:border-[#6A7282]'>
+          <div className='p-2 flex flex-col gap-2'>
+            {themeOptions.map((option) => (
+              <button
+                onClick={handleClick}
+                className={`w-full px-2 py-2 text-left text-sm font-bold ${theme === option ? ' bg-gray-300 dark:bg-[#010101]' : ''} text-gray-700 hover:bg-gray-200 dark:text-gray-200 dark:hover:bg-[#1A1A1A] flex gap-2 items-center rounded-xl capitalize cursor-pointer`}
+                value={option}
+                key={option}
+              >
+                <img
+                  src={
+                    theme === 'light'
+                      ? `/${option}BorderBlackIcon.svg`
+                      : `/${option}Icon.svg`
+                  }
+                  alt='Theme Icon'
+                />
+                {option}
+              </button>
+            ))}
           </div>
         </div>
       )}
